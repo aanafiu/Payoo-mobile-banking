6ed42fc4ca7da3d0305d6e7cd9e9e6cfe0d80d1c
aanafiu <abdullahall.nafiu@gmail.com>
2024-09-21 03:24:12 +0600

    Complete Add Money & Cashout & Transaction

--- js/manager.js
+++ js/manager.js
@@ -1,4 +1,6 @@
-console.log("i am universal23");
+
+
+// Add Money
 document.getElementById("addMoneyBtn").addEventListener('click', function(event){
     event.preventDefault();
 
@@ -11,7 +13,7 @@ document.getElementById("addMoneyBtn").addEventListener('click', function(event)
     {
         const presentBalance = document.getElementById('mainBalance').innerText;
        
-        console.log(presentBalance, newBalance);
+        // console.log(presentBalance, newBalance);
         const addBalance = document.getElementById('mainBalance');
 
         addBalance.innerText = parseFloat(presentBalance) + parseFloat(newBalance);
@@ -24,4 +26,32 @@ document.getElementById("addMoneyBtn").addEventListener('click', function(event)
         alert("Give Proper Information")
     }
     
+})
+
+console.log("i am cashout");
+// Cash Out
+document.getElementById('cashoutBtn').addEventListener('click', function(event){
+    event.preventDefault();
+
+    const agentNumber = getValue('agentNumber');
+    const cashoutAmount = getValue('cashoutAmount');
+    const withdrawPass = getValue('withdrawPass');
+    const presentBalance = document.getElementById('mainBalance').innerText;
+
+
+    if( !isNaN(agentNumber)  && agentNumber.toString().length === 11 && withdrawPass.toString().length === 4 && parseFloat(presentBalance) > parseFloat(cashoutAmount))
+        {
+            
+            // console.log(presentBalance, newBalance);
+            const addBalance = document.getElementById('mainBalance');
+    
+            addBalance.innerText = parseFloat(presentBalance) - parseFloat(cashoutAmount);
+    
+            addNewTransaction('cashout', agentNumber, cashoutAmount);
+            
+        }
+        else
+        {
+            alert("Give Proper Information")
+        }
 })
--- js/universalFunction.js
+++ js/universalFunction.js
@@ -1,4 +1,4 @@
-console.log("i am universal");
+// console.log("i am universal");
 function getValue(id)
 {
     const idValue = document.getElementById(id).value;
@@ -28,4 +28,87 @@ function addNewTransaction(typeOfWay,bankList, balance)
         transactionText.appendChild(newText);
         
     }
+    else  if(typeOfWay === 'cashout')
+        {
+            const transactionText = document.getElementById('listOfTransaction');
+    
+            const newText = document.createElement('div');
+            newText.innerHTML = `
+                             <div class="flex justify-between items-center gap-2 text-base bg-white rounded-xl p-2">
+                                <div class="flex justify-center items-center gap-2">
+                                    <img src="./assets/wallet.png" alt="">
+                                <h1 class="uppercase grow" >${typeOfWay}</h1>
+                                </div>
+                                <h1>${bankList} <br> Agent</h1>
+                                <h1 class="font-extrabold text-red-500">-${balance} Taka</h1>
+                            </div>
+            `;
+            transactionText.appendChild(newText);
+            
+        }
 }
+
+
+// button toggle
+
+document.getElementById("addMoney").addEventListener("click", function(){
+
+    document.getElementById("addMoneySection").classList.remove("hidden");
+
+    document.getElementById("transactionSection").classList.add("hidden");
+    document.getElementById("transaction").classList.remove("active");
+    document.getElementById("cashOutMoney").classList.remove("active");
+    document.getElementById("cashoutSection").classList.add("hidden");
+    const clr = document.getElementById("addMoney");
+    // console.log(clr.classList.contains("active"));
+    if(clr.classList.contains("active"))
+    {
+        clr.classList.remove("active");
+    }
+    else{
+        clr.classList.add("active");
+    }
+    
+
+})
+document.getElementById("cashOutMoney").addEventListener("click", function(){
+
+    document.getElementById("cashoutSection").classList.remove("hidden");
+    document.getElementById("addMoneySection").classList.add("hidden");
+    document.getElementById("transactionSection").classList.add("hidden");
+    document.getElementById("transaction").classList.remove("active");
+    document.getElementById("addMoney").classList.remove("active");
+
+    const clr = document.getElementById("cashOutMoney");
+    // console.log(clr.classList.contains("active"));
+    if(clr.classList.contains("active"))
+    {
+        clr.classList.remove("active");
+    }
+    else{
+        clr.classList.add("active");
+    }
+    
+})
+
+document.getElementById("transaction").addEventListener("click", function(){
+
+    document.getElementById("addMoney").classList.remove("active");
+    document.getElementById("cashOutMoney").classList.remove("active");
+    document.getElementById("cashoutSection").classList.add("hidden");
+    document.getElementById("addMoneySection").classList.add("hidden");
+
+    document.getElementById("transactionSection").classList.remove("hidden");
+
+    const clr = document.getElementById("transaction");
+    // console.log(clr.classList.contains("active"));
+    if(clr.classList.contains("active"))
+    {
+        clr.classList.remove("active");
+    }
+    else{
+        clr.classList.add("active");
+    }
+    
+    
+})
